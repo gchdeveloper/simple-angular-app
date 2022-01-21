945c66d185696c87fb2b2764c4db260949bf5a68
gchdeveloper <gchdeveloper@gmail.com>
2022-01-21 11:25:42 -0800

    restored build scripts

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 
-echo 'Can it even echo deliver.sh?'
+echo 'Install Angular CLI for ng serve'
+set -x
+npm install -g @angular/cli
+set +x
+
+echo 'Start and save pid to kill later'
+set -x
+npm start &
+sleep 1
+echo $! > .pidfile
+set +x
+
+echo 'App running on http://localhost:4200'
